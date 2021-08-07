735dc8788b7102ed613cf0186335ac20d29162fe
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-08-07 14:24:53 -0600

    feature/schemas basic outline

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,2 @@
+// Query ( me, user, users, roadtrip, roadtrips )
+// Mutation (login, signup, createUser, addUser, removeUser, addRoadtrip, deleteRoadtrip, addExpense, updateExpense, deleteExpense, addImage, deleteImage, addStop, deleteStop)
