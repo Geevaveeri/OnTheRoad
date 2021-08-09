13a65befae73a123a2807a13a4794285640f68a2
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-08-09 13:51:06 -0600

    basic header component with logout and login nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
-// header image
-// nav bar
-    // home 
-    // log in / log out
+const Header = () => {
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
+    return (
+        <header>
+          <div>
+            <Link to="/">
+              <h1>Deep Thoughts</h1>
+            </Link>
+    
+            <nav>
+              {Auth.loggedIn() ? (
+                <>
+                  <a href="/" onClick={logout}>
+                    Logout
+                  </a>
+                </>
+              ) : (
+                <>
+                  <Link to="/login">Login</Link>
+                </>
+              )}
+            </nav>
+          </div>
+        </header>
+      );
+};
+
+export default Header;
+// add image to header/nav
