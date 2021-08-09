a81f8e01ae7fe0e3b68451f8b3ad7e57c9286ff8
kotaewing <kota.ewing@gmail.com>
2021-08-09 10:24:49 -0600

    moved config to server.js

--- server/utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-const fileupload = require('express-fileupload');
-
-cloudinary.config({
-    cloud_name: process.env.CC_NAME,
-    api_key: process.env.CC_KEY,
-    api_secret: process.env.CC_SECRET
-});
-
