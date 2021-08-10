d15d97ca4e9be976dc605617aabffe814a1d3bf0
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-08-09 20:20:45 -0600

    fix spacing for header and footer prelim

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,37 +2,77 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 
+// Material UI imports
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+const logo = require('../assets/images/logo.png');
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 500
+    },
+});
+
 const Dashboard = () => {
+    const classes = useStyles();
+
     const logout = event => {
         event.preventDefault();
         Auth.logout();
     };
 
     return (
-        <div>
+        <div className='dashboard'>
+            {Auth.loggedIn() ? (
+                <>
+                    <h1>Welcome to On The Road!</h1>
+                    <Link to="/roadtrips">
+                        My Roadtrips
+                            </Link>
+                </>
+            ) : (
+                    <>
+                        <Grid
+                            container
+                            spacing={0}
+                            direction="column"
+                            alignItems="center"
+                            justify="center"
+                            style={{ minHeight: '100vh' }}
+                        >
+                            <Box class='newSignUp'>
 
-            <header>
-                <div>
-                    {Auth.loggedIn() ? (
-                        <>
-                            <h1>
-                                Welcome to On The Road!
-                            </h1>
-                            <Link to="/roadtrips">
-                                My Roadtrips
-                            </Link>
-                            <Link to="/profile">
-                                My Profile
-                            </Link>
-                        </>
-                    ) : (
-                            <>
-                                <h1>Join your first road trip today!</h1>
-                                <Link to="/signup">Sign Up</Link>
-                            </>
-                        )}
-                </div>
-            </header>
+                                <Card className={classes.root}>
+                                    <CardActionArea>
+                                        <div>
+                                            <img src={logo.default}></img>
+                                        </div>
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                New to On The Road?
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary" component="p">
+                                                Join and create your first road trip today!
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                    <CardActions>
+                                        <Grid item xs={12}>
+                                            <Link to="/signup">Sign Up</Link>
+                                        </Grid>
+                                    </CardActions>
+                                </Card>
+                            </Box>
+                        </Grid>
+                    </>
+                )}
         </div>
     );
 };
