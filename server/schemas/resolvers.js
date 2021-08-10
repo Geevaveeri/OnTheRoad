3bda0304f341953f5306ea2609e3c711b98d4a26
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-08-09 22:48:44 -0600

    fixed addUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -156,4 +156,4 @@ const resolvers = {
 module.exports = resolvers;
 
 // Query ( *me, *user, *users, *roadtrip, *roadtrips )
-// Mutation (*login, signup, *createUser, *addUser,*removeUser, *addRoadtrip, *deleteRoadtrip, addExpense, updateExpense, deleteExpense, addImage, deleteImage, addStop, deleteStop)
+// Mutation (*login, signup, *createUser, *addUser,*removeUser, *addRoadtrip, *deleteRoadtrip, addExpense, updateExpense, deleteExpense, *addImage, deleteImage, addStop, deleteStop)
