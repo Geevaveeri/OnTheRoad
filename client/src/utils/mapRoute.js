71ff26a82a6646790fbf922e9d0405762cf22b21
kotaewing <kota.ewing@gmail.com>
2021-08-09 19:08:39 -0600

    added fetch routes for trueway api

--- client/src/utils/mapRoute.js
+++ client/src/utils/mapRoute.js
@@ -1,3 +1,23 @@
-async function addressToCoord(address) {
+const addressToCoord = async (address) => {
+    const fetchCoord = await fetch(`https://trueway-geocoding.p.rapidapi.com/Geocode?address=${param}&language=en`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "b38f645fa0msh490534a54dae3fbp1fc29bjsn703e822658db",
+            "x-rapidapi-host": "trueway-geocoding.p.rapidapi.com"
+        }
+    })
 
+    return await fetchCoord.json();
+}
+
+const getOptRoute = async (coords) => {
+    const fetchRoute = await fetch(`https://trueway-directions2.p.rapidapi.com/FindDrivingRoute?stops=${param}&optimize=true`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "b38f645fa0msh490534a54dae3fbp1fc29bjsn703e822658db",
+            "x-rapidapi-host": "trueway-directions2.p.rapidapi.com"
+        }
+    })
+
+    return await fetchRoute.json();
 }
