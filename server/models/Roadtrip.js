a3b2b11fbfe5ace2d6a3380c41e595b778dce7e8
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-08-12 20:30:22 -0600

    completed server schema resolvers

--- server/models/Roadtrip.js
+++ server/models/Roadtrip.js
@@ -3,39 +3,34 @@ const stopSchema = require("./Stop");
 const expenseSchema = require("./Expense");
 const imageSchema = require("./Image");
 
-const roadtripSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        start: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        destination: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        playlist: {
-            type: String,
-            trim: true
-        },
-        images: [imageSchema],
-        expenses: [expenseSchema],
-        stops: [stopSchema],
-        users: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ],
-
-    }
-);
+const roadtripSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	start: {
+		type: String,
+		trim: true,
+	},
+	destination: {
+		type: String,
+		trim: true,
+	},
+	playlist: {
+		type: String,
+		trim: true,
+	},
+	images: [imageSchema],
+	expenses: [expenseSchema],
+	stops: [stopSchema],
+	users: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+	],
+});
 
 const Roadtrip = model("Roadtrip", roadtripSchema);
 
