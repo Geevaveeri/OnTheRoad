ebcd20d035f5d401030703a44cd85244f9b5c433
Sharni Zaugg <sharni.greener@gmail.com>
2021-08-16 20:24:36 -0600

    heroku

--- client/src/utils/CloudinaryUploadWidget.js
+++ client/src/utils/CloudinaryUploadWidget.js
@@ -27,8 +27,8 @@ const CloudinaryUploadWidget = params => {
 
     var myWidget = window.cloudinary.createUploadWidget(
         {
-            cloudName: "dne2ggd07",
-            uploadPreset: "rnnidcq5",
+            cloudName: process.env.REACT_APP_CC_NAME,
+            uploadPreset: process.env.REACT_APP_CC_PRESET,
             sources: ['local', 'url', 'facebook', 'instagram', 'google_drive']
         },
         (error, result) => {
